4dcf0a5f5ee91835dd37f099cfaf6cd938c16c51
Kimo0104 <55680839+Kimo0104@users.noreply.github.com>
2022-10-21 16:30:39 -0600

    Models (#1)
    * models initialized
    
    * added nullable fields
    
    * removed accidental import

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,7 +1,54 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
-class Teacher(models.Model):
-    name = models.CharField(max_length=80)
-    age = models.IntegerField()
+class Authors(models.Model):
+    host = models.CharField(max_length=255)
+    display_name = models.CharField(max_length=32)
+    github_url = models.CharField(max_length=255, blank = True, null = True)
+    profile_image = models.CharField(max_length=255, blank = True, null = True)
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+    accepted = models.BooleanField(default=False)
+
+class Posts(models.Model):
+    PLAINTEXT = 'PT'
+    COMMONMARK = 'CM'
+    choices = [
+        (PLAINTEXT, 'PLAINTEXT'),
+        (COMMONMARK, 'COMMONMARK')
+    ]
+    title = models.CharField(max_length = 64)
+    description_type = models.CharField(
+        max_length = 2,
+        choices = choices,
+        default = PLAINTEXT, 
+        blank = True, 
+        null = True  
+    )
+    description = models.TextField(blank = True, null = True)
+    date = models.DateField()
+    image_url = models.CharField(max_length=255, blank = True, null = True)
+    author = models.ForeignKey(Authors, on_delete= models.CASCADE)
+
+class Comments(models.Model):
+    comment = models.TextField()
+    commenter = models.ForeignKey(Authors, on_delete = models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete = models.CASCADE)
+
+class Likes(models.Model):
+    liker = models.ForeignKey(Authors, on_delete = models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete = models.CASCADE)
+
+class Follows(models.Model):
+    PENDING = 0
+    FOLLOWING = 1
+    choices = [
+        (PENDING, 'Pending') ,
+        (FOLLOWING, 'Following')   
+    ]
+    relationship_state = models.IntegerField(
+        choices = choices,
+        default = PENDING
+    )
+    author = models.ForeignKey(Authors, on_delete= models.CASCADE, related_name = "follower")
+    foreign_author = models.ForeignKey(Authors, on_delete= models.CASCADE, related_name="followed")
+
