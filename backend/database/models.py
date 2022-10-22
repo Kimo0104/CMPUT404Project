1418c2d73b3aefa8b75ad73b3dad94b5a495cab7
Kimo0104 <55680839+Kimo0104@users.noreply.github.com>
2022-10-21 22:36:53 -0600

    er diagram to be fixed (#3)
    * er diagram to be fixed
    
    * Completed ER diagram
    
    * edited posts and authors
    
    * fixed models
    
    * fixed bugs
    
    * fix typo
    
    * fixed typo
    
    * new init migration
    
    Co-authored-by: Tomas <tomas.peschke@gmail.com>

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,54 +1,87 @@
 from django.db import models
 
 class Authors(models.Model):
-    host = models.CharField(max_length=255)
-    display_name = models.CharField(max_length=32)
-    github_url = models.CharField(max_length=255, blank = True, null = True)
-    profile_image = models.CharField(max_length=255, blank = True, null = True)
-    username = models.CharField(max_length=32)
-    password = models.CharField(max_length=32)
-    accepted = models.BooleanField(default=False)
+    id = models.CharField(max_length = 255, primary_key = True)
+    type = models.CharField(max_length = 255, default = "author")
+    host = models.CharField(max_length = 255)
+    displayName = models.CharField(max_length = 32)
+    url = models.CharField(max_length = 255)
+    github = models.CharField(max_length = 255)
+    accepted = models.BooleanField(default = True)
+    profileImage = models.CharField(max_length = 255)
 
 class Posts(models.Model):
+    PLAINTEXT = 'text/plain'
+    MARKDOWN = 'text/markdown'
+    PUBLIC = 'PUBLIC'
+    FRIENDS = 'FRIENDS'
+    content_type_choices = [
+        (PLAINTEXT, 'PLAINTEXT'),
+        (MARKDOWN, 'COMMONMARK')
+    ]
+    visibility_choices = [
+        (PUBLIC, 'PUBLIC'),
+        (FRIENDS, 'FRIENDS')
+    ]
+    id = models.CharField(max_length = 255, primary_key = True)
+    type = models.CharField(max_length = 255, default = "author")
+    title = models.CharField(max_length = 255)
+    source = models.CharField(max_length = 255)
+    origin = models.CharField(max_length = 255)
+    description = models.CharField(max_length = 255)
+    contentType = models.CharField(max_length = 15, choices = content_type_choices, default = PLAINTEXT, blank = True, null = True)
+    content = models.TextField()
+    author = models.ForeignKey(Authors, on_delete= models.CASCADE)
+    count = models.IntegerField(default = 0)
+    comments = models.CharField(max_length = 255)
+    published = models.DateTimeField()
+    visibility = models.CharField(max_length = 8, choices = visibility_choices, default = PUBLIC)
+    unlisted = models.BooleanField(default = False)
+
+
+class Followers(models.Model):
+    followed = models.ForeignKey(Authors, on_delete= models.CASCADE, related_name = "follower")
+    follower = models.ForeignKey(Authors, on_delete= models.CASCADE, related_name="followed")
+
+
+class FollowRequests(models.Model):
+    reciever = models.ForeignKey(Authors, on_delete= models.CASCADE, related_name = "reciever")
+    requester = models.ForeignKey(Authors, on_delete= models.CASCADE, related_name = "requester")
+
+
+class Comments(models.Model):
     PLAINTEXT = 'PT'
-    COMMONMARK = 'CM'
+    MARKDOWN = 'CM'
     choices = [
         (PLAINTEXT, 'PLAINTEXT'),
-        (COMMONMARK, 'COMMONMARK')
+        (MARKDOWN, 'MARKDOWN')
     ]
-    title = models.CharField(max_length = 64)
-    description_type = models.CharField(
+    id = models.CharField(max_length=255, primary_key = True)
+    type = models.CharField(max_length=16)
+    author = models.ForeignKey(Authors, on_delete = models.CASCADE)
+    comment = models.CharField(max_length=255)
+    contentType = models.CharField(
         max_length = 2,
         choices = choices,
         default = PLAINTEXT, 
         blank = True, 
-        null = True  
+        null = True
     )
-    description = models.TextField(blank = True, null = True)
-    date = models.DateField()
-    image_url = models.CharField(max_length=255, blank = True, null = True)
-    author = models.ForeignKey(Authors, on_delete= models.CASCADE)
-
-class Comments(models.Model):
-    comment = models.TextField()
-    commenter = models.ForeignKey(Authors, on_delete = models.CASCADE)
-    post = models.ForeignKey(Posts, on_delete = models.CASCADE)
+    published = models.DateField()
 
 class Likes(models.Model):
-    liker = models.ForeignKey(Authors, on_delete = models.CASCADE)
-    post = models.ForeignKey(Posts, on_delete = models.CASCADE)
+    id = models.CharField(max_length=255, primary_key = True)
+    context = models.CharField(max_length=255)
+    summary = models.CharField(max_length=64)
+    type = models.CharField(max_length=16)
+    author = models.ForeignKey(Authors, on_delete = models.CASCADE)
 
-class Follows(models.Model):
-    PENDING = 0
-    FOLLOWING = 1
-    choices = [
-        (PENDING, 'Pending') ,
-        (FOLLOWING, 'Following')   
-    ]
-    relationship_state = models.IntegerField(
-        choices = choices,
-        default = PENDING
-    )
-    author = models.ForeignKey(Authors, on_delete= models.CASCADE, related_name = "follower")
-    foreign_author = models.ForeignKey(Authors, on_delete= models.CASCADE, related_name="followed")
+class Liked(models.Model):
+    id = models.CharField(max_length=255, primary_key = True)
+    author = models.ForeignKey(Authors, on_delete = models.CASCADE)
+    like = models.ForeignKey(Likes, on_delete = models.CASCADE)
 
+class Inbox(models.Model):
+    id = models.CharField(max_length=255, primary_key = True)
+    author = models.ForeignKey(Authors, on_delete = models.CASCADE)
+    post_id = models.ForeignKey(Posts, on_delete = models.CASCADE)
