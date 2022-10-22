b7f54d275f094f56b79b1ee318ca3f001498ccda
Kimo0104 <55680839+Kimo0104@users.noreply.github.com>
2022-10-22 16:15:52 -0600

    new migration and fixed null checks

--- backend/database/models.py
+++ backend/database/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Authors(models.Model):
     id = models.CharField(max_length = 255, primary_key = True)
-    type = models.CharField(max_length = 255, default = "author", blank=True)
+    type = models.CharField(max_length = 255, default = "author", blank=True, null = True)
     host = models.CharField(max_length = 255)
     displayName = models.CharField(max_length = 32)
     url = models.CharField(max_length = 255)
     github = models.CharField(max_length = 255)
-    accepted = models.BooleanField(blank = True, default = True)
+    accepted = models.BooleanField(blank = True, default = True, null = True)
     profileImage = models.CharField(max_length = 255)
 
 class Posts(models.Model):
@@ -24,7 +24,7 @@ class Posts(models.Model):
         (FRIENDS, 'FRIENDS')
     ]
     id = models.CharField(max_length = 255, primary_key = True)
-    type = models.CharField(max_length = 255, default = "post", blank=True)
+    type = models.CharField(max_length = 255, default = "post", blank=True, null = True)
     title = models.CharField(max_length = 255)
     source = models.CharField(max_length = 255)
     origin = models.CharField(max_length = 255)
@@ -32,10 +32,10 @@ class Posts(models.Model):
     contentType = models.CharField(max_length = 15, choices = content_type_choices, default = PLAINTEXT, blank = True, null = True)
     content = models.TextField()
     author = models.ForeignKey(Authors, on_delete= models.CASCADE)
-    count = models.IntegerField(default = 0, blank = True)
-    published = models.DateTimeField(auto_now_add=True, blank=True)
-    visibility = models.CharField(max_length = 8, choices = visibility_choices, default = PUBLIC)
-    unlisted = models.BooleanField(default = False, blank = True)
+    count = models.IntegerField(default = 0, blank = True, null = True)
+    published = models.DateTimeField(auto_now_add=True, blank=True, null = True)
+    visibility = models.CharField(max_length = 8, choices = visibility_choices, default = PUBLIC, null = True, blank = True)
+    unlisted = models.BooleanField(default = False, blank = True, null = True)
 
 
 class Followers(models.Model):
@@ -58,12 +58,12 @@ class Comments(models.Model):
         (MARKDOWN, 'MARKDOWN')
     ]
     id = models.CharField(max_length=255, primary_key = True)
-    type = models.CharField(max_length=16, default = "comment", blank=True)
+    type = models.CharField(max_length=16, default = "comment", blank=True, null = True)
     author = models.ForeignKey(Authors, on_delete = models.CASCADE)
     post = models.ForeignKey(Posts, on_delete = models.CASCADE)
     comment = models.CharField(max_length=255)
     contentType = models.CharField(
-        max_length = 2,
+        max_length = 15,
         choices = choices,
         default = PLAINTEXT, 
         blank = True, 
@@ -75,7 +75,7 @@ class Likes(models.Model):
     id = models.CharField(max_length=255, primary_key = True)
     context = models.CharField(max_length=255)
     summary = models.CharField(max_length=64)
-    type = models.CharField(max_length=16, default = "like", blank = True)
+    type = models.CharField(max_length=16, default = "like", blank = True, null = True)
     published = models.DateTimeField(auto_now_add=True, blank=True)
     author = models.ForeignKey(Authors, on_delete = models.CASCADE)
     post = models.ForeignKey(Posts, on_delete = models.CASCADE)
@@ -84,7 +84,7 @@ class LikesComments(models.Model):
     id = models.CharField(max_length=255, primary_key = True)
     context = models.CharField(max_length=255)
     summary = models.CharField(max_length=64)
-    type = models.CharField(max_length=16, default = "likescomment", blank=True)
+    type = models.CharField(max_length=16, default = "likescomment", blank=True, null = True)
     published = models.DateTimeField(auto_now_add=True, blank=True)
     author = models.ForeignKey(Authors, on_delete = models.CASCADE)
     comment = models.ForeignKey(Comments, on_delete = models.CASCADE)
