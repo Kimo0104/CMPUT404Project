dfe4c181c74008af2951076775630b8ec527dfdc
Tomas <tomas.peschke@gmail.com>
2022-10-22 01:16:07 -0600

    Initial views skeleton

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,14 +17,43 @@ from email.mime import base
 from django.contrib import admin
 from django.urls import path
 from rest_framework import routers
-from database.views import PostsAPIs
+from database.views import PostsAPIs, CommentsAPIs, LikesAPIs, LikedAPIs, InboxAPIs
 
 urlpatterns = [
+    #admin
     path('admin/', admin.site.urls),
+
+    #posts
+    path('service/posts/<str:authorId>/posts/<str:postId>', PostsAPIs.as_view({"get": "getPost"})),
+    path('service/posts/<str:authorId>/posts/<str:postId>', PostsAPIs.as_view({"post": "udpatePost"})),
+    path('service/posts/<str:authorId>/posts/<str:postId>', PostsAPIs.as_view({"delete": "deletePost"})),
+    path('service/posts/<str:authorId>/posts/<str:postId>', PostsAPIs.as_view({"put": "createPost"})),
+    path('service/posts/<str:authorId>/posts/<str:postId>/image', PostsAPIs.as_view({"get": "getImagePost"})),
+
+    #comments
+    path('service/authors/<str:authorId>/posts/<str:postId>/comments', CommentsAPIs.as_view({"get": "getComments"})),
+    path('service/authors/<str:authorId>/posts/<str:postId>/comments', CommentsAPIs.as_view({"get": "createComment"})),
+
+    #likes
+    path('service/authors/<str:authorId>/posts/<str:postId>/likes', LikesAPIs.as_view({"get": "getPostLikes"})),
+    path('service/authors/<str:authorId>/posts/<str:postId>/comments/<str:commentId>/likes', LikesAPIs.as_view({"get": "getCommentLikes"})),
+
+    #liked
+    path('service/authors/<str:authorId>/likes', LikedAPIs.as_view({"get": "getAuthorLiked"})),
+
+    #inbox
+    path('service/authors/<str:authorId>/inbox', InboxAPIs.as_view({"get": "getInbox"})),
+    path('service/authors/<str:authorId>/inbox', InboxAPIs.as_view({"post": "sendPost"})),
+    path('service/authors/<str:authorId>/inbox', InboxAPIs.as_view({"post": "deleteInbox"})),
+
 ]
 
 router = routers.SimpleRouter()
-router.register(r'posts', PostsAPIs, basename = 'posts')
+router.register(r'service/posts', PostsAPIs, basename = 'service/posts')
+router.register(r'service/authors', CommentsAPIs, basename = 'service/authors')
+router.register(r'service/authors', LikesAPIs, basename = 'service/authors')
+router.register(r'service/authors', LikedAPIs, basename = 'service/authors')
+router.register(r'service/authors', InboxAPIs, basename = 'service/authors')
 
 
 urlpatterns = router.urls
--- backend/database/views.py
+++ backend/database/views.py
@@ -7,14 +7,14 @@ from rest_framework.response import Response
 from django.http import HttpResponseBadRequest
 import json
 
-from .models import Posts                          #Models defines how their objects are stored in the database
-from .serializers import PostsSerializer #serializers defines how to convert a post object to JSON
+#Models defines how their objects are stored in the database
+#serializers defines how to convert a post object to JSON
+from .models import Posts, Comments, Likes, Liked, Inbox
+from .serializers import PostsSerializer, CommentsSerializer, LikesSerializer, LikedSerializer, InboxSerializer
 import datetime
 
 class PostsAPIs(viewsets.ViewSet):
-    #query
-    #serializer_class = PostsSerializer
-    
+
     """-----------------------------------------------
     --------------------------------------------------
     ---------- START OF DEFAULT FUNCTIONS ------------
@@ -115,26 +115,13 @@ class PostsAPIs(viewsets.ViewSet):
     --------- START OF SPECIFIC FUNCTIONS ------------
     --------------------------------------------------
     -----------------------------------------------"""
-    #GET posts/{id}/author will call this function
-    #this function returns all public posts from the requested author
-    @action(detail=True, methods=['get'])
-    def author(self, request, pk=None):
-        queryset = Posts.objects.raw("""
-            YOUR SQL HERE
-        """)
-        
-        serializer = PostsSerializer(queryset, many=True)
-        return Response(serializer.data)
-        
 
-
-    #GET posts/{id}/comments will call this function
-    #this function returns all 
-    @action(detail=True, methods=['get']) 
-    #   detail states whether an {id) needed. if detail were false, then the URL would be: posts/comments
-    #   methods states which HTTP requests will call this action
-    #   the name of the function determines the last part of the URL
-    def comments(self, request, pk=None):
+    #GET service/authors/{AUTHOR_ID/posts/{POST_ID}
+    #get the public post whose id is POST_ID
+    @action(detail=True, methods=['get'],)
+    def getPost(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        postId = kwargs["postId"]
         queryset = Posts.objects.raw("""
             YOUR SQL HERE
         """)
@@ -142,8 +129,12 @@ class PostsAPIs(viewsets.ViewSet):
         serializer = PostsSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    @action(detail=True, methods=['get'])
-    def stream(self, request, pk=None):
+    #POST service/authors/{AUTHOR_ID/posts/{POST_ID}
+    #update the post whose id is POST_ID (must be authenticated)
+    @action(detail=True, methods=['post'],)
+    def updatePost(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        postId = kwargs["postId"]
         queryset = Posts.objects.raw("""
             YOUR SQL HERE
         """)
@@ -151,12 +142,177 @@ class PostsAPIs(viewsets.ViewSet):
         serializer = PostsSerializer(queryset, many=True)
         return Response(serializer.data)
 
+    #DELETE service/authors/{AUTHOR_ID/posts/{POST_ID}
+    #remove the post whose id is POST_ID
+    @action(detail=True, methods=['delete'],)
+    def deletePost(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        postId = kwargs["postId"]
+        queryset = Posts.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = PostsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    #PUT service/authors/{AUTHOR_ID/posts/{POST_ID}
+    #create a post where its id is POST_ID
+    @action(detail=True, methods=['put'],)
+    def createPost(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        postId = kwargs["postId"]
+        queryset = Posts.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = PostsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    #GET service/authors/{AUTHOR_ID}/posts/{POST_ID}/image
+    #get the public post converted to binary as an image
+    @action(detail=True, methods=['get'],)
+    def getImagePost(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        postId = kwargs["postId"]
+        queryset = Posts.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = PostsSerializer(queryset, many=True)
+        return Response(serializer.data)
     
     """-----------------------------------------------
     --------------------------------------------------
     --------- END OF SPECIFIC FUNCTIONS --------------
     --------------------------------------------------
     -----------------------------------------------"""
+
+class CommentsAPIs(viewsets.ViewSet):
+
+    #GET service/authors/{AUTHOR_ID}/posts/{POST_ID}/comments
+    #get the list of comments of the post whose id is POST_ID
+    @action(detail=True, methods=['get'],)
+    def getComments(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        postId = kwargs["postId"]
+        queryset = Comments.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = CommentsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    #POST service/authors/{AUTHOR_ID}/posts/{POST_ID}/comments
+    #if you post an object of “type”:”comment”, it will add your comment to the post whose id is POST_ID
+    @action(detail=True, methods=['post'],)
+    def createComment(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        postId = kwargs["postId"]
+        queryset = Comments.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = CommentsSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class LikesAPIs(viewsets.ViewSet):
+
+    #POST service/authors/{AUTHOR_ID}/inbox/
+    #send a like object to AUTHOR_ID
+    @action(detail=True, methods=['post'],)
+    def sendLike(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        queryset = Likes.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = LikesSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    #GET service/authors/{AUTHOR_ID}/posts/{POST_ID}/likes
+    #a list of likes from other authors on AUTHOR_ID’s post POST_ID
+    @action(detail=True, methods=['get'],)
+    def getPostLikes(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        postId = kwargs["postId"]
+        queryset = Likes.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = LikesSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    #GET service/authors/{AUTHOR_ID}/posts/{POST_ID}/comments/{COMMENT_ID}/likes
+    #a list of likes from other authors on AUTHOR_ID’s post POST_ID comment COMMENT_ID
+    @action(detail=True, methods=['get'],)
+    def getCommentLikes(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        postId = kwargs["postId"]
+        commentId = kwargs["commentId"]
+        queryset = Likes.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = LikesSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class LikedAPIs(viewsets.ViewSet):
+
+    #GET service/authors/{AUTHOR_ID}/liked
+    #list what public things AUTHOR_ID liked
+    @action(detail=True, methods=['get'],)
+    def getAuthorLiked(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        queryset = Liked.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = LikedSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class InboxAPIs(viewsets.ViewSet):
+
+    #GET service/authors/{AUTHOR_ID}/inbox
+    #if authenticated get a list of posts sent to AUTHOR_ID (paginated)
+    @action(detail=True, methods=['get'],)
+    def getInbox(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        queryset = Inbox.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = InboxSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    #POST service/authors/{AUTHOR_ID}/inbox
+    #send a post to the author
+    @action(detail=True, methods=['post'],)
+    def sendPost(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        queryset = Inbox.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = InboxSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    #DELETE service/authors/{AUTHOR_ID}/inbox
+    #clear the inbox
+    @action(detail=True, methods=['delete'],)
+    def deleteInbox(self, request, *args, **kwargs):
+        authorId = kwargs["authorId"]
+        queryset = Inbox.objects.raw("""
+            YOUR SQL HERE
+        """)
+        
+        serializer = InboxSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+
+    
+
+    
 
 
 
