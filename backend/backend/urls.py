2e8a8ab4280dbf79fde4df8771a845f0177f73be
Kimo0104 <55680839+Kimo0104@users.noreply.github.com>
2022-10-22 14:13:45 -0600

    removed unused imports

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,10 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from email.mime import base
 from django.contrib import admin
 from django.urls import path
-from rest_framework import routers
 from database.views import PostsAPIs, CommentsAPIs, LikesAPIs, LikedAPIs, InboxAPIs
 
 urlpatterns = [
