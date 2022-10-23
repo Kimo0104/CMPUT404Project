c820d7308816d964c74778b3c23c3f232e1f981a
Chee Tey <cheetiantey@gmail.com>
2022-10-22 21:00:03 -0600

    Fix FollowRequest receiver name + register models in admin.py

--- backend/database/admin.py
+++ backend/database/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Authors, Posts, Followers, FollowRequests, Comments, Likes, LikesComments, Liked, Inbox
 
 # Register your models here.
+admin.site.register(Authors)
+admin.site.register(Posts)
+admin.site.register(Followers)
+admin.site.register(FollowRequests)
+admin.site.register(Comments)
+admin.site.register(Likes)
+admin.site.register(LikesComments)
+admin.site.register(Liked)
+admin.site.register(Inbox)
