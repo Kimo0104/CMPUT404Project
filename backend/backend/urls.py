52e26c241ad0b967b12b59dec787834d6fd3cf2b
Chee Tey <37930535+cheetiantey@users.noreply.github.com>
2022-10-24 13:11:38 -0600

    Following APIs (#16)
    * Added uuidGenerator and getCurrentDate function. Partial progress on views requests
    
    * Untested APIs
    
    * a
    
    * Post API updates
    
    * Bug Fixes
    
    * Fix FollowRequest receiver name + register models in admin.py
    
    * Fix receiver spelling in FollowRequests table
    
    * Follow + FollowRequests WIP
    
    * Add requestToFollow MVP
    
    * Add a follower MVP
    
    Co-authored-by: Tomas <tomas.peschke@gmail.com>

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -54,12 +54,13 @@ urlpatterns = [
     #followRequests
     path('service/authors/<str:authorId>/followRequest', FollowRequestsAPIs.as_view({"get": "getFollowRequests"})),
     path('service/authors/<str:authorId>/followRequest/<str:foreignAuthorId>', FollowRequestsAPIs.as_view({"delete": "removeRequest"})),
-    path('service/authors/<str:authorId>/followers/<str:foreignAuthorId>', FollowRequestsAPIs.as_view({"post": "requestToFollow"})),
+    # path('service/authors/<str:authorId>/followers/<str:foreignAuthorId>', FollowRequestsAPIs.as_view({"post": "requestToFollow"})),
 
     #follows
     path('service/authors/<str:authorId>/followers', FollowsAPIs.as_view({"get": "getFollowers"})),
     path('service/authors/<str:authorId>/followers/<str:foreignAuthorId>', FollowsAPIs.as_view({
         "get": "checkFollower",
         "put": "addFollower",
-        "delete": "removeFollower"})),
+        "delete": "removeFollower",
+        "post": "requestToFollow"})),
 ]
