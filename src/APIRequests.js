2cefde83a6253853b923e8c8c98e0607c04034a3
Kimo0104 <55680839+Kimo0104@users.noreply.github.com>
2022-10-24 23:40:19 -0600

    home page

--- src/APIRequests.js
+++ src/APIRequests.js
@@ -4,6 +4,6 @@ export const SERVER_URL = process.env.SERVER_URL || "http://localhost:8000"
 
 export const getPost  = async (authorId, postId) => {
     const path = SERVER_URL + `/authors/${authorId}/posts/${postId}`
-    const response = await axios.get(`${path}`,{params: {teacherId: teacherId} });
+    const response = await axios.get(`${path}`,{params: {authorId: authorId, postId: postId} });
     return response.data.result;
 };
