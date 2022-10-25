b0729a2952a91de2e71884fc29bc55dc78a0dd8a
Kimo0104 <55680839+Kimo0104@users.noreply.github.com>
2022-10-25 00:12:37 -0600

    Home Page And Top Bar Component (#22)
    * home page
    
    * updatedhandle publush

--- src/APIRequests.js
+++ src/APIRequests.js
@@ -4,6 +4,12 @@ export const SERVER_URL = process.env.SERVER_URL || "http://localhost:8000"
 
 export const getPost  = async (authorId, postId) => {
     const path = SERVER_URL + `/authors/${authorId}/posts/${postId}`
-    const response = await axios.get(`${path}`,{params: {teacherId: teacherId} });
+    const response = await axios.get(`${path}`,{params: {authorId: authorId, postId: postId} });
     return response.data.result;
 };
+
+export const createPost = async (authorId, postId, data) => {
+    const path = SERVER_URL + `/authors/${authorId}/posts/${postId}`
+    const response = await axios.put(`${path}`, null,  {params: data});
+    return response.data.result;
+}
