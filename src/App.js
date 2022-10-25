ea8e713ff1034245d13dce479d06f8d16bfe268a
Mahajabin Tokon <tokonislam1122@gmail.com>
2022-10-25 16:05:08 -0600

    front-end done, I guess

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Login from './components/login/Login.jsx'
+// import Login from './components/login/Login.jsx'
 import Home from './components/home/Home.jsx'
+import { Login } from "./components/login/Login";
+import { Register } from "./components/login/Register";
+
 function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
   return (
     <BrowserRouter>
       <div className="App">
         <div className = "content">
+              {
+                currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
+              }
             <Routes>
-              <Route path="/" element={<Login/>}/>
+              {/* <Route path="/" element={<Login/>}/> */}
               <Route path="home" element={<Home/>}/>
             </Routes>
         </div>
