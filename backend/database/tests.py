6e525ed4dc63ca35c642833d8db2b93fde45c410
Chee Tey <cheetiantey@gmail.com>
2022-11-03 14:01:33 -0600

    Uncomment AccountsTest

--- backend/database/tests.py
+++ backend/database/tests.py
@@ -9,50 +9,50 @@ from .models import Posts, Authors, FollowRequests, Followers
 from django.db.utils import IntegrityError
 
 
-# class AccountsTest(APITestCase):
-#     def setUp(self):
-#         # We want to go ahead and originally create a user. 
-#         self.test_user = User.objects.create_user('testuser', 'test@example.com', 'testpassword')
+class AccountsTest(APITestCase):
+    def setUp(self):
+        # We want to go ahead and originally create a user. 
+        self.test_user = User.objects.create_user('testuser', 'test@example.com', 'testpassword')
 
-#         # URL for creating an account.
-#         self.create_url = reverse('account-create')
+        # URL for creating an account.
+        self.create_url = reverse('account-create')
 
-#     def test_create_user_with_preexisting_email(self):
-#         data = {
-#             'username': 'testuser2',
-#             'email': 'test@example.com',
-#             'password': 'testuser'
-#         }
+    def test_create_user_with_preexisting_email(self):
+        data = {
+            'username': 'testuser2',
+            'email': 'test@example.com',
+            'password': 'testuser'
+        }
 
-#         response = self.client.post(self.create_url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-#         self.assertEqual(User.objects.count(), 1)
-#         self.assertEqual(len(response.data['email']), 1)
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['email']), 1)
 
-#     def test_create_user_with_invalid_email(self):
-#         data = {
-#             'username': 'foobarbaz',
-#             'email':  'testing',
-#             'passsword': 'foobarbaz'
-#         }
+    def test_create_user_with_invalid_email(self):
+        data = {
+            'username': 'foobarbaz',
+            'email':  'testing',
+            'passsword': 'foobarbaz'
+        }
 
 
-#         response = self.client.post(self.create_url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-#         self.assertEqual(User.objects.count(), 1)
-#         self.assertEqual(len(response.data['email']), 1)
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['email']), 1)
 
-#     def test_create_user_with_no_email(self):
-#         data = {
-#                 'username' : 'foobar',
-#                 'email': '',
-#                 'password': 'foobarbaz'
-#         }
+    def test_create_user_with_no_email(self):
+        data = {
+                'username' : 'foobar',
+                'email': '',
+                'password': 'foobarbaz'
+        }
 
-#         response = self.client.post(self.create_url, data, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-#         self.assertEqual(User.objects.count(), 1)
-#         self.assertEqual(len(response.data['email']), 1)
+        response = self.client.post(self.create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(len(response.data['email']), 1)
 
 
 class PostsTests(TestCase):
