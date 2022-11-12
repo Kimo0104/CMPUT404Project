10a4d54b4db3b952fcb4f49a47a4a1aac8827409
Mahajabin Tokon <tokonislam1122@gmail.com>
2022-11-12 09:05:00 -0700

    Email changed to confirm password

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -4,21 +4,28 @@ import { createUser } from '../../APIRequests'
 
 
 export default function Register() {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
+    // const [email, setEmail] = useState('');
+    const [password, setPass] = useState('');
+    const [confirmPassword, setConfirmedPass] = useState('');
     const [name, setName] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        let data = {
-            // Do NOT change. Leave key as "displayName"
-            "displayName": name,
-            "email": email,
-            "password": pass
-          }
-        createUser(data);
-        navigate('/login')
+        if (password !== confirmPassword) {
+            alert("Passwords don't match");
+        }
+        else{
+            let data = {
+                // Do NOT change. Leave key as "displayName"
+                "displayName": name,
+                "email": email,
+                "password": pass
+            }
+            createUser(data);
+            navigate('/login')
+        }
+            
     };
     
     return (
@@ -29,11 +36,15 @@ export default function Register() {
                     <label htmlFor="name">username</label>
                     <input value={name} onChange={(e) => setName(e.target.value)}name="name" id="name" placeholder="username" />
 
-                    <label htmlFor="email">email</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+                    {/* <label htmlFor="email">email</label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" /> */}
 
                     <label htmlFor="password">password</label>
                     <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+
+                    <label htmlFor="password">comfirm password</label>
+                    <input value={pass} onChange={(e) => setConfirmedPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+
                     <button type="submit">Register</button>
                 </form>
                 <div className="link-btn">
