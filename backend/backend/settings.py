57881beb2b2b89f2e8ffbdbcdf8aa91976e2b50b
Chee Tey <cheetiantey@gmail.com>
2022-11-13 16:53:49 -0700

    Deal with staticfiles

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,8 +138,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'build/static')
-    '/app/build/static'
+    os.path.join(BASE_DIR, 'build/static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
