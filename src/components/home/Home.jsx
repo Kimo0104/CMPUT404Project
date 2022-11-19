7303a3563ba8dd43c7537dc17570430d4a41281c
Kimo0104 <55680839+Kimo0104@users.noreply.github.com>
2022-11-19 16:32:26 -0700

    re added friendrequestlist

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,7 +3,11 @@ import Grid from '@mui/material/Grid';
 import TopBar from '../topbar/TopBar.jsx'
 import HomeTab from '../homeTab/HomeTab'
 import PublishButton from "./PublishButton.jsx";
+import FriendRequestList from "../friendRequestList/FriendRequestList";
+import { userIdContext } from '../../App';
 export default function Home() {
+  const userId = React.useContext(userIdContext);
+  let requestList = <FriendRequestList authorId={userId} />
   return (
     <div>
       <TopBar/>
