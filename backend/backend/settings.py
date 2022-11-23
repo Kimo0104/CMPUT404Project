4c4a99e421518ff96064af499c8329c63729f484
Chee Tey <cheetiantey@gmail.com>
2022-11-22 22:21:52 -0700

    Modify settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'assets')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
