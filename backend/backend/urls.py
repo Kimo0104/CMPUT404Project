9e44da24ac44d4abb7bb71c695a928f5a0812fa5
Chee Tey <cheetiantey@gmail.com>
2022-11-22 23:40:39 -0700

    Add function to urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,10 +19,31 @@ from database.views import AuthorsAPIs, PostsAPIs, CommentsAPIs, LikesAPIs, Like
 from rest_framework import permissions
 from django.views.generic import TemplateView
 
-import sys
-sys.path.insert(1, 'backend/database/views')
-from views import FrontendAppView
-
+# from ..database.views import FrontendAppView
+# from views import FrontendAppView
+from django.views import View
+import os
+from django.http import HttpResponse
+class FrontendAppView(View):
+    """
+    Serves the compiled frontend entry point (only works if you have run `yarn
+    build`).
+    """
+    index_file_path = os.path.join(os.path.dirname(__file__), 'build', 'index.html')
+    def get(self, request):
+        try:
+            with open(self.index_file_path) as f:
+                return HttpResponse(f.read())
+        except FileNotFoundError:
+            return HttpResponse(
+                """
+                This URL is only used when you have built the production
+                version of the app. Visit http://localhost:3000/ instead after
+                running `yarn start` on the frontend/ directory
+                """,
+                status=501,
+            )
+        
 # https://www.jasonmars.org/2020/04/22/add-swagger-to-django-rest-api-quickly-4-mins-without-hiccups/
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
