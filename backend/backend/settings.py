6cdfddd7c1f3487e772a3104b445a34be851f064
Chee Tey <cheetiantey@gmail.com>
2022-11-23 11:47:56 -0700

    Add one more slash to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(Path(__file__).resolve().parent.parent.parent, 'build/static')
