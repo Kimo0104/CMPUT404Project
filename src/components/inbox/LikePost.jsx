87dd93a073c2b38a680a15ec9e1e2bd54a55d24f
Tomas <64938432+Frotaboom@users.noreply.github.com>
2022-12-07 14:58:24 -0700

    Image post like (#175)
    * allow deletion of non-public posts and changed message in likes
    
    * fixed comments on image posts
    
    Co-authored-by: Tomas <tomas.peschke@gmail.com>

--- src/components/inbox/LikePost.jsx
+++ src/components/inbox/LikePost.jsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
 export default function BasicCard(props) {
   //props has summary, context
   //likes show the title of the post with a desc "User liked this post!"
 
+  var message = `${props.summary}: ${props.context}`
+  if (!context || context === "") {
+    message = props.summary;
+  }
+
   return (
     <Card sx={{ minWidth: 275 }} style={{backgroundColor: "#FAF9F6"}}> 
       <Typography sx={{ fontSize: 18, marginLeft: 3 }} color="text.primary" align='left'>
-        {props.summary}: {props.context}
+        {message}
       </Typography>
     </Card>
   );
